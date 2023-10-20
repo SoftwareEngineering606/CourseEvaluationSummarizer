2a8334de0ef7e024d0f7377c2cf2b563224fdc6d
Kangdong <kky5082@tamu.edu>
2023-10-20 11:17:14 -0500

    rspec

--- spec/requests/excel_sheets_controller_spec.rb
+++ spec/requests/excel_sheets_controller_spec.rb
@@ -1,30 +1,30 @@
-# spec/controllers/pages_controller_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
   let(:file_path) { 'app/assets/images/SampleExcel.xlsx' }
   let(:excel_parser) { ExcelParser.new(file_path) }
 
-  describe 'parse_data' do
-    it 'parses data from the Excel file' do
+  describe 'GET #generate' do
+    it 'redirects to download_report_path' do
       get :generate
       expect(response).to redirect_to(download_report_path)
     end
   end
-  describe "GET #validate" do
-    it "redirects to download report path" do
+
+  describe 'GET #validate' do
+    it 'redirects to download_report_path' do
       get :validate
       expect(response).to redirect_to(download_report_path)
     end
   end
-end
 
-RSpec.describe PagesController, type: :controller do
-  describe 'GET download' do
+  describe 'GET #download' do
     context 'when the Excel file exists' do
       it 'sends the Excel file for download' do
+        allow(File).to receive(:exist?).and_return(true)
+
         get :download
+
         expect(response).to be_successful
         expect(response.headers['Content-Type']).to eq('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         expect(response.headers['Content-Disposition']).to include('attachment; filename="grouped_data.xlsx"')
@@ -34,7 +34,9 @@ RSpec.describe PagesController, type: :controller do
     context 'when the Excel file does not exist' do
       it 'sets a flash message and redirects to the root path' do
         allow(File).to receive(:exist?).and_return(false)
+
         get :download
+
         expect(flash[:alert]).to eq('The Excel file does not exist.')
         expect(response).to redirect_to(root_path)
       end
