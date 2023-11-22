3890f70f03c667c1b34f58182d2efa1bbdd05ed9
Pratik Manoj Desai <pratikdesai@Pratiks-MacBook-Pro.local>
2023-11-21 19:59:39 -0600

    model changed to turbo-16k

--- app/services/chatgpt_service.rb
+++ app/services/chatgpt_service.rb
@@ -21,7 +21,7 @@ class ChatgptService
 
   def call
     body = {
-      model: 'gpt-3.5-turbo', 
+      model: 'gpt-3.5-turbo-16k', 
       messages: [{ role: 'user', content: query }]
     }
     response = HTTParty.post(api_url, body: body.to_json, headers: options[:headers], timeout: 500)
